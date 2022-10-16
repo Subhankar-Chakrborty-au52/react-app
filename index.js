952ed2d85a39b81b3c6febcf02507f218ef031e1
Subhankar chakrborty <subhankarchakrborty@gmail.com>
2022-10-16 13:56:57 +0530

    initial rest api created

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello git learner")
+const express = require("express")
+
+const app = express()
+
+app.get("/check", (req, res) => {
+    res.send("Health check ok")
+})
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001")
+})
